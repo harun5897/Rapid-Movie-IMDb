d99746572fa2defbbb1b4ef87b67547e9ed3955e
harun5897 <harungb025@gmail.com>
2022-12-16 19:19:51 +0700

    feat(frontend): add template vite + vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="mt-8 ml-4">
+    <router-view></router-view>
+  </div>
+</template>
---
+++ frontend/src/views/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <span class="bg-red-500 text-white p-3 rounded-full border">This is Component index</span>
+  </div>
+</template>
